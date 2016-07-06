23f299a5fd02927d37f8ccbf0d785acb67daa2f2
nori <nori@nori-MacBook-Pro.local>
2016-07-06 20:26:55 +0900

    controllers and routes except favorites, comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "comics#index"
-
-
+  resources :comics, only: [:index, :show]
+  resources :users, only: :show
+  resources :sites, only: [:index, :show]
 end
