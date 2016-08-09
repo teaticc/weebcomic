359b872e90233a3ea5b0dfb8bcf94ff8c2d46934
nori <nori@nori-MacBook-Pro.local>
2016-08-09 19:33:16 +0900

    Rename css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_sites
 
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :nickname
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :nickname
+  end
+
+  private
+  def set_sites
+    @sites = Site.all
+  end
 end
