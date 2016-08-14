4cc7bc85c663439d26827477e5f1990aabe8fd88
nori <nori@nori-MacBook-Pro.local>
2016-08-14 17:34:00 +0900

    simplify js

--- app/assets/javascripts/comics.js
+++ app/assets/javascripts/comics.js
@@ -1,66 +1,25 @@
-// window.addEventListener("load",function(){
-//    // 曜日取得
-//   var day_checker = new Array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
-//   var weekday = new Date().getDay();
-//   var today = day_checker[weekday];
-//   // 取得した曜日タグとIDにselected付与
-//   $("li#" + today).addClass("selected");
-//   $("div." + today).addClass("selected");
-//   // タブクリックで選択曜日にslected付与
-//   $("ul.tab-selector li").on("click",function(){
-//     $("li.selected").removeClass("selected");
-//     $(this).addClass("selected");
-//     $("div.wday_content").hide();
-//     $("div." + this.id).show();
-//   });
-// });
-// var ready;
-// ready =function(){
-//    // 曜日取得
-//   var day_checker = new Array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
-//   var weekday = new Date().getDay();
-//   var today = day_checker[weekday];
-//   // 取得した曜日タグとIDにselected付与
-//   $("li#" + today).addClass("selected");
-//   $("div." + today).addClass("selected");
-//   // タブクリックで選択曜日にslected付与
-//   $("ul.tab-selector li").on("click",function(){
-//     $("li.selected").removeClass("selected");
-//     $(this).addClass("selected");
-//     $("div.wday_content").hide();
-//     $("div." + this.id).show();
-//   });
-// };
-
-
 $(document).on("turbolinks:load", function(){
-  var day_checker = new Array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
-  var weekday = new Date().getDay();
-  var today = day_checker[weekday];
-  // 取得した曜日タグとIDにselected付与
-  $("li#" + today).addClass("selected");
-  $("div." + today).addClass("selected");
-  // タブクリックで選択曜日にslected付与
-  $("ul.tab-selector li").on("click",function(){
-    $("li.selected").removeClass("selected");
-    $(this).addClass("selected");
-    $("div.wday_content").hide();
-    $("div." + this.id).show();
+  selectToday();
+  $(".tab__selector li").on("click",function(event){
+    tabChange(event);
   });
-  // お気に入り解除ボタン
-  $(document).on("click", "input.like.yes",function(){
-    console.log(this);
-    $(this).removeClass("yes");
-    $("input.not_like#" + this.id).addClass("yes");
-    $(this).hide();
-    $("input.not_like.yes#" + this.id).show();
-  });
-  // お気に入りボタン
-  $(document).on("click", "input.not_like.yes",function(){
-    console.log(this);
-    $(this).removeClass("yes");
-    $("input.like#" + this.id).addClass("yes");
-    $(this).hide();
-    $("input.like.yes#" + this.id).show();
-  });
+
 });
+
+// 今日のタブを選択
+function selectToday(){
+  var day_checker = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  var today = new Date().getDay();
+  var weekday = day_checker[today];
+  // 取得した曜日タグとIDにselected付与
+  $("li#" + weekday).addClass("selected");
+  $("div." + weekday).addClass("selected");
+}
+
+// 選択されたタブを表示
+function tabChange(event){
+  $("li.selected").removeClass("selected");
+  $(event.target).addClass("selected");
+  $("div.wday-content").hide();
+  $("div." + event.target.id).show();
+}
